d8b19ea572e08b62e8f944d40b938ba029ccd36b
momen210 <touristmomen@gmail.com>
2023-12-26 20:04:07 +0600

    add visited country list

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,18 +5,38 @@ import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
+  const handleVisitedCountry = (country) => {
+    console.log("Add this to your visited country");
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
+      <div className="left">
+        <h5>Visited Countries: {visitedCountries.length}</h5>
+        <ul>
+          {
+            visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+          }
+        </ul>
+      </div>
       <div className="country-container">
         {countries.map((soloCountry) => (
-          <Country key={soloCountry.cca3} country={soloCountry}></Country>
+          <Country
+            key={soloCountry.cca3}
+            country={soloCountry}
+            handleVisitedCountryProps={handleVisitedCountry}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Box.css"
-export default function Country({country}){
+export default function Country({country, handleVisitedCountryProps}){
     console.log(country);
     const {name, continents, population, flags} = country;
 
@@ -10,12 +10,16 @@ export default function Country({country}){
         setVisited(!visited)
     }
 
+    console.log(handleVisitedCountryProps);
+
     return (
-        <div className="box">
+        <div className={`box ${visited ? 'visited' : 'none'}`}>
             <h3 className="name">Name: {name?.common}</h3>
             <p>Continents: {continents}</p>
             <p>Population: {population}</p>
             <img src={flags?.png} alt="" />
+            <button onClick={()=> handleVisitedCountryProps(country)}>Mark Visited</button>
+            <br />
             <button onClick={handleVisited} className="margin-right">{visited? "Visited" : "Go"}</button>
             {visited? "Congratulations You visit " + name?.common +'.' : "Click to Visit"}
         </div>
