657928afbb9fe00b7952507f1c51fa3635b28918
momen210 <touristmomen@gmail.com>
2023-12-27 12:28:11 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Countries from "./components/Countries/Countries";
 function App() {
   return (
     <>
-      <h2>React World Tour</h2>
+      <h2>React World Awesome Tour</h2>
       <Countries></Countries>
     </>
   );
